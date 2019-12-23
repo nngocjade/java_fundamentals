84e735fd2cecb243fe5666f9bf388d7a64e490b4
nngocjade <nngocjade@gmail.com>
2019-12-23 12:30:25 -0600

    done

--- src/labs_examples/datatypes_operators/labs/Exercise_01.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.datatypes_operators.labs;
 
+import sun.management.snmp.jvmmib.JVM_MANAGEMENT_MIBOidTable;
+
 /**
  * Data Types and Operators Exercise 1: Variable declaration and Initialization
  *
@@ -16,8 +18,21 @@ public class Exercise_01 {
 
     public static void main(String[] args) {
 
-        // write code here
+        int iNum = 53237954;
 
+        System.out.println(" the value of int is " + iNum);
+
+        double dNum = 34534634699998567785565645646456765.4456;
+
+        System.out.println(" the value of double is " + dNum);
+
+        float fNum = 456566332446.4456f;
+
+        System.out.println(" the value of float is " + fNum);
+
+        char c = 'j';
+
+        System.out.println(" the value of char is " + c);
     }
 }
 
