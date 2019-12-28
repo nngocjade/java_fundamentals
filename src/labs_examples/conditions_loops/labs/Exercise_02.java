90a22b58313fef851d88e06e2c52f5978994dd23
nngocjade <nngocjade@gmail.com>
2019-12-27 23:49:27 -0600

    Gotttt ittt yess!!!

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -27,25 +27,30 @@ public class Exercise_02 {
         int number = scanner.nextInt();
 
         if (number > 0 && number <= 7) {
-            if (number == 1) {
-                System.out.println("Monday");
-            }
-            if (number == 2) {
-                System.out.println("Tuesday");
-            }
-            if (number == 3) {
-                System.out.println("Wednesday");
-            }
-            if (number == 4) {
-                System.out.println("Thursday");
-            }
-            if (number == 5) {
-                System.out.println("Friday");
-            }
-            if (number == 6) {
-                System.out.println("Saturday");
-            }
-            if (number == 7) {
+            if (number < 7) {
+                if (number < 4){
+                    if (number == 1) {
+                        System.out.println("Monday");
+                    }
+                    if (number == 2){
+                        System.out.println("Tuesday");
+                    }
+                    if (number == 3) {
+                        System.out.println("Wednesday");
+                    }
+                } else{
+                    if (number == 4) {
+                        System.out.println("Thursday");
+                    }
+                }
+
+                if (number == 5) {
+                    System.out.println("Friday");
+                }
+                if (number == 6) {
+                    System.out.println("Saturday");
+                }
+            } else {
                 System.out.println("Sunday");
             }
         } else if (number < 0 || number > 7){
