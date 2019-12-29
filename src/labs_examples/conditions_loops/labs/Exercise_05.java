9a276247414001ed9dd785904f7c6bc298eb674c
nngocjade <nngocjade@gmail.com>
2019-12-29 17:28:16 -0600

    working on ex 5

--- src/labs_examples/conditions_loops/labs/Exercise_05.java
+++ src/labs_examples/conditions_loops/labs/Exercise_05.java
@@ -1,5 +1,5 @@
 package labs_examples.conditions_loops.labs;
-
+import java.util.Scanner;
 /**
  * Conditions and Loops Exercise 5: Calculator
  *
@@ -16,4 +16,16 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Enter a number: ");
+        int upper = scanner.nextInt();
+        System.out.println("Enter a number: ");
+        int lower = scanner.nextInt();
+
+        int x = upper + lower;
+        for (x) {
+            System.out.println("lower + upper = " +);
+        }
+    }
 }
