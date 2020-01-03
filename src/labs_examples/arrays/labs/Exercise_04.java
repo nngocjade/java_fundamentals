ec3d81a2801a6601bf6352320d1dcbd683d290f6
nngocjade <nngocjade@gmail.com>
2020-01-03 13:17:05 -0600

    tweaked ex 5, done with ex 4

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -1,5 +1,5 @@
 package labs_examples.arrays.labs;
-
+import java.util.Arrays;
 /**
  *  Irregular Arrays
  *
@@ -9,5 +9,41 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_04 {
+    public static void main(String[] args) {
+        //declaring 2D array with 5 rows
+        int [][] irreg_array = new int [5][];
+
+        //First row has 4 columns
+        irreg_array [0] = new int [4];
+        //Second row has 7 columns
+        irreg_array [1] = new int [7];
+        //Third row has 2 columns
+        irreg_array [2] = new int [2];
+        //fourth row has 3 columns
+        irreg_array [3] = new int [3];
+        //fifth row has 2 columns
+        irreg_array [4] = new int [2];
+
+
+        //Initializing array
+        int count = 0;
+        for (int i = 0; i < irreg_array.length; i++){
+            for (int k = 0; k < irreg_array[i].length; k++){
+                irreg_array[i][k] = count++;
+            }
+        }
+        //Printing the elements for each row and column
+        System.out.println("Content of 2D irregular array ");
+        for (int i = 0; i < irreg_array.length; i++){
+            for (int k = 0; k < irreg_array[i].length; k++){
+                System.out.println(irreg_array[i][k]);
+
+            }
+            System.out.println();
+        }
+
+
+
+    }
 
 }
