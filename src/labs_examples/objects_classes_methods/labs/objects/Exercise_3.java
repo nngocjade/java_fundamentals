fcfc5aab7d180238f31f74037ff61018d9a4376a
nngocjade <nngocjade@gmail.com>
2020-01-07 15:18:56 -0600

    ex 3 done

--- src/labs_examples/objects_classes_methods/labs/objects/Exercise_3.java
+++ src/labs_examples/objects_classes_methods/labs/objects/Exercise_3.java
@@ -13,47 +13,49 @@ package labs_examples.objects_classes_methods.labs.objects;
 //each with a different constructor
 public class Exercise_3 {
     public static void main(String[] args) {
-        Jean person = new Jean ("Bob", "Jimmy", "Drew");
-        Jean 
+        Jean sizeChart = new Jean("S", 6, 36, 28.5, 38);
+        Jean fit = new Jean(27, "BoyFriend");
+        Jean bodyType = new Jean("S", "PL", "Pe");
     }
-
-
-
 }
 
 
 //a POJO
 //3 overloaded constructors
 class Jean{
+    //size chart
     String size;
     int numSize;
     int bust;
     double waist;
     int hip;
 
+    //style or fit
     int inseam;
     String style;
 
-    String personA;
-    String personB;
-    String personC;
+    //body type - (standard, plus, petit)
+    String standard;
+    String plusSize;
+    String petit;
+
 
     public Jean(String size, int numSize, int bust, double waist, int hip){
         this.size = size;
-        this.numSize =numSize;
+        this.numSize = numSize;
         this.bust = bust;
         this.waist = waist;
         this.hip = hip;
-
     }
     public Jean(int inseam, String style){
         this.inseam = inseam;
         this.style = style;
     }
-    public Jean(String personA, String personB, String personC){
-       this.personA = personA;
-       this.personB = personB;
-       this.personC = personC;
+    public Jean(String standard, String plusSize, String petit){
+        this.standard = standard;
+        this.plusSize = plusSize;
+        this.petit = petit;
     }
+
 }
 
