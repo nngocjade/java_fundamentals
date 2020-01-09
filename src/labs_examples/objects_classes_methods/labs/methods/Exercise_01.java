984a0c26e83c043d3f52f13534a674a8c18561f1
nngocjade <nngocjade@gmail.com>
2020-01-08 22:51:36 -0600

    in progress

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
@@ -6,22 +6,36 @@ package labs_examples.objects_classes_methods.labs.methods;
 public class Exercise_01 {
 
     public static void main(String[] args) {
-
         // please create the methods as instructed below then
         // call each of those methods from here, within the main()
+        multiply(3,5);
+        divide(4,6);
+        System.out.println("Why did the grocery delivery guy get fired?");
+        joke();
 
     }
 
 
     // 1) Create a static multiply() method below that takes two int arguments (int a, int b) and
     //    returns the result of a * b
-
+    public static void multiply(int a, int b) {
+        int multiply = a * b;
+        System.out.println("a * b = " + multiply);
+    }
 
     // 2) Create a static divide() method below that takes two int arguments (int a, int b) and
     //    returns the result of a / b
-
+    public static void divide(int a, int b) {
+        int divide = a / b;
+        System.out.println("a / b = " + divide);
+    }
 
     // 3) Create a static void method that will print of joke of your choice to the console
+    public static String joke() {
+        String aJoke = new String ("He drove people bananas!");
+        System.out.println(aJoke);
+        return aJoke;
+    }
 
 
     // 4) Create a static method that takes in a number in years (int years) as an argument
