24f74cb00c0ee2e9a48e254937cafc4732bfc979
nngocjade <nngocjade@gmail.com>
2020-01-11 15:49:15 -0600

    in progress

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_3.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_3.java
@@ -1,5 +1,6 @@
 package labs_examples.objects_classes_methods.labs.methods;
 
+import java.util.Arrays;
 /**
  * Create a new class called MethodTraining.java and in this class please do the following:
  *     1) Demonstrate method overloading in this class
@@ -25,13 +26,17 @@ public class Exercise_3 {
         int largest = varargs(2,4,33,55,3,1);
         //invoke consonants
         String consonantLength = consonants("green");
+        //invoke HighestLowest
+        Pair highLow = HighestLowest(4,5,2,9,12,55,45,32);
 
         //print varargs
-        System.out.print(largest);
-        System.out.println();
+        System.out.print("Largest value: " + largest);
         //print consonant
-        System.out.println("Number of consonants: " + consonantLength);
-
+        System.out.println("\n" + "Number of consonants: " + consonantLength);
+        //print Highest Lowest
+        //System.out.println(highLow);
+        System.out.println("Highest value: " + highLow.high);
+        System.out.println("Lowest value: " + highLow.low);
     }
     //Create a method that will return the largest of 4 numbers (all of which are passed in as arguments)
     //not using static here because it is under the same class as controller
@@ -59,15 +64,49 @@ public class Exercise_3 {
     //Write a method that will print some kind of ascii art to the console (Google "ascii art" if needed)
     public void printAscii(String[] args) {
 
+
+
+
+
+
+
+
     }
     //Write a method that will determine whether or not a number is prime
     public void primeOrNot(String[] args) {
 
+
+
+
+
+
+
+
     }
     //Write a method that will return a small array containing the highest and lowest numbers in a given numeric array,
     // *             which is passed in as an argument
-    public static void HighestLowest(String[] args) {
+    //TODO Ask Ryan about this
+    static Pair HighestLowest( int...array) {
 
+        Pair highLow = new Pair();
+        int i;
+
+        if (array[0] > array[1]){
+            highLow.high = array[0];
+            highLow.low = array[1];
+        }
+        else {
+            highLow.high = array[1];
+            highLow.low = array[0];
+        }
+        for (i = 2; i < array.length; i++) {
+            if (array[i] > highLow.high) {
+                highLow.high = array[i];
+            } else if (array[i] < highLow.low) {
+                highLow.low = array[i];
+            }
+        }
+        return highLow;
     }
     //Write a method that takes 3 arguments (int maxNum, int divisor1, int divisor2) and returns an Integer Arraylist.
     // *         In this method create an Integer ArrayList and populate it with each number between zero and maxNum that is
@@ -75,13 +114,34 @@ public class Exercise_3 {
     // *         length of the returned list
     public static void Arraylist(String[] args) {
 
+
+
+
+
+
+
+
+
     }
     //Write a method that will reverse an array in place use only one extra temp variable. For this exercise you cannot
     // *         instantiate a second array. You must reverse the array in place using only one extra temp variable. Hint: this
     // *         variable is used to temporarily store individual values in the array
     public static void ReverseArray(String[] args) {
 
+
+
+
+
+
+
+
+
     }
 
+}
+
+class Pair{
+    int low;
+    int high;
 }
 
