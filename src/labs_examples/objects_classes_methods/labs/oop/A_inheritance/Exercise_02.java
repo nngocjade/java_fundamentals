90926d6ddac5a5b9520e95c536872389eb2f08c0
nngocjade <nngocjade@gmail.com>
2020-01-16 19:54:07 -0600

    done for now

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -3,7 +3,9 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Run the class below and study the output. What prints and why?
  *
- * Your answer:
+ * Your answer: i = 20 is printed - because object B, with the name " a" , is being called from the controller method.
+ * Object B, in this case, is sub-class B. Meanwhile, B inherited i from superclass A, int i now has a new value of 20.
+ * So, when B is being called upon will return 20.
  *
  */
 class A {
