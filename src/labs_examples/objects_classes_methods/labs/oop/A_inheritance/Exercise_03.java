5d31827b6ec649a44ab7206d589b3c602e41a38c
nngocjade <nngocjade@gmail.com>
2020-01-17 23:04:15 -0600

    done

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
@@ -3,7 +3,16 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Why does the output print in the order it does?
  *
- * You answer:
+ * You answer: Due to the order of execution, the static blocks are executed first,
+ *             followed by constructors and then the instance methods (a non-static method of a class).
+ *
+ *             But in an inheritance relationship, the order of execution of constructors is
+ *             parent class to child class.
+ *
+ *             In this case, although, the main method (which has a static keyword) should print first
+ *             but it is invoking the constructor in subclass C_1
+ *             therefore, the order of execution of constructors in inheritance relationship kicks in.
+ *             So, the parent class A_1 prints first, then following subclass B_1, lastly subclass C_1.
  *
  */
 
