080e40898ec4821233c849f8c4df482887d6923b
nngocjade <nngocjade@gmail.com>
2020-01-19 22:10:04 -0600

    almost done

--- src/labs_examples/static_nonstatic/labs/StaticNonStatic_Exercise_1.java
+++ src/labs_examples/static_nonstatic/labs/StaticNonStatic_Exercise_1.java
@@ -18,19 +18,33 @@ public class StaticNonStatic_Exercise_1 {
         staticMethod();
         int divide = divide(12,6);
         System.out.println(divide);
+
         //same class - static calling non-static
         StaticNonStatic_Exercise_1 object = new StaticNonStatic_Exercise_1();
         object.nonStaticMethod();
         StaticNonStatic_Exercise_1 multiply = new StaticNonStatic_Exercise_1();
         multiply.multiply(22.22,33.33);
+
         //static calling non-static - another class
         System.out.println("printing static method calling a non-static method - another class ");
-        Backpack bag = new Backpack("A Book Bag");
+        Backpack type = new Backpack();
+        type.backPackType("Book bag");
+
         // static calling static - another class
         System.out.println("printing static method calling a static method - another class ");
-        Backpack.portableOrNot();
+        Backpack.isPortable();
 
+        //static main method calling nonStatic1 to print nonStatic2 calling from nonStatic1 - same class
+        System.out.println("printing (static main method calling nonStatic1) to print (non-static multiply calling from nonStatic1)");
+        StaticNonStatic_Exercise_1 nonStaticNonStatic = new StaticNonStatic_Exercise_1();
+        nonStaticNonStatic.nonStatic1();
 
+        //A non-static method calling a non-static method in another class
+        StaticNonStatic_Exercise_1 nonStaticNonStaticAnotherClass = new StaticNonStatic_Exercise_1();
+        nonStaticNonStaticAnotherClass.nonStatic2();
+
+        //A non-static method calling a static method in the same class
+        
     }
     public static void staticMethod(){
         System.out.println("printing static method calling static method - same class");
@@ -46,29 +60,40 @@ public class StaticNonStatic_Exercise_1 {
         System.out.println(x);
     }
     public void nonStatic1(){
-        nonStatic2();
+        multiply(6,3);
     }
     public void nonStatic2(){
-        System.out.println("(static main method calling nonStatic1) to print (nonStatic2 calling from nonStatic1)");
+        System.out.println("printing static main method calling nonStatic2 - nonStatic2 calling non-static backPackTyp1 from another class - non-static backPackType1 calling non-static backPackType and non-static isNotPortable");
+        Backpack type2 = new Backpack();
+        type2.backPackType1();
     }
+
 
 
 }
 class Backpack {
-    String type;
-    static int strap;
-    static String portable;
-    static boolean isNotPortable;
+    protected String type;
+    protected static int strap;
+    protected static String portable;
+    protected static String notPortable;
 
     public Backpack(){
     }
-    public Backpack(String type) {
-        this.type = type;
+
+    public void backPackType(String type) {
         System.out.println(type);
     }
-    public static void portableOrNot(){
+    public void backPackType1(){
+        backPackType("not a backpack");
+        isNotPortable();
+    }
+    public void isNotPortable(){
+        notPortable = "is not portable";
+        System.out.println(notPortable);
+    }
+    public static void isPortable(){
         portable = "is portable";
-
         System.out.println(portable);
     }
+
 }
