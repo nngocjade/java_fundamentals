66f164d530e3a7436f475f97182d2770a042cf56
nngocjade <nngocjade@gmail.com>
2020-01-20 22:33:44 -0600

    done

--- src/labs_examples/packages/labs/labs_examples1/Example1.java
+++ src/labs_examples/packages/labs/labs_examples1/Example1.java
@@ -1,10 +1,19 @@
 package labs_examples.packages.labs.labs_examples1;
 
+import labs_examples.packages.labs.labs_examples.Example;
+
 public class Example1 {
     public static void main(String[] args) {
 
-    }
-    public static void multiply() {
+        //calling multiply method from the same package and class
+        Example1 m = new Example1();
+        System.out.println("calling multiply method from the same package and class: "+ m.multiply(3,5));
 
+        //calling divide method from another package
+        int divide = Example.divide(18,2);
+        System.out.println("calling divide method from another package: " + divide);
+    }
+    protected int multiply(int x, int y) {
+        return x*y;
     }
 }
