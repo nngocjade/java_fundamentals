a5a0dcecdaa8edfc1b490d0c0401ad00bf512f79
nngocjade <nngocjade@gmail.com>
2020-01-20 21:52:07 -0600

    in progress

--- src/labs_examples/packages/labs/labs_examples/Example.java
+++ src/labs_examples/packages/labs/labs_examples/Example.java
@@ -1,4 +1,17 @@
 package labs_examples.packages.labs.labs_examples;
 
-public class example {
+import labs_examples.packages.labs.labs_examples1.Example1;
+
+public class Example {
+    public static void main(String[] args) {
+        //
+        //calling protected multiply method from another class
+        //Example1.multiply(4,5);
+        //cannot call the multiply method because it has a protected access
+        // - meaning it can only be accessed by members of the same class.
+        System.out.println(divide(3,2));
+    }
+    public static int divide(int a, int b){
+        return a*b;
+    }
 }
