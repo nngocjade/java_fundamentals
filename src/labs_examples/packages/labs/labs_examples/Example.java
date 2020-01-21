7f470eb8522ee10b27a42e89d167f62a3c62b5dd
nngocjade <nngocjade@gmail.com>
2020-01-20 22:32:35 -0600

    done

--- src/labs_examples/packages/labs/labs_examples/Example.java
+++ src/labs_examples/packages/labs/labs_examples/Example.java
@@ -4,14 +4,18 @@ import labs_examples.packages.labs.labs_examples1.Example1;
 
 public class Example {
     public static void main(String[] args) {
-        //
-        //calling protected multiply method from another class
+        //calling divide method from the same package and class
+        System.out.println("calling divide method from the same package and class: " + divide(6,2));
+        System.out.println("cannot call multiply method from another package because the multiply method has protected access" +
+                " meaning it can only be accessed by members within the same package.");
+        //calling protected multiply method from another package
+
         //Example1.multiply(4,5);
+
         //cannot call the multiply method because it has a protected access
-        // - meaning it can only be accessed by members of the same class.
-        System.out.println(divide(3,2));
+        // - meaning it can only be accessed by members within the same package
     }
     public static int divide(int a, int b){
-        return a*b;
+        return a/b;
     }
 }
