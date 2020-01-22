88cfc79826318d06e4b652aa17a03b77dac03698
nngocjade <nngocjade@gmail.com>
2020-01-21 23:18:15 -0600

    done

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -7,4 +7,24 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+class Exercise_2{
+    public static void main(String[] args) {
 
+        int [] a = anArray(0,6,22,54,12);
+
+        try{
+            int x = a[4] / a[0]; //ArrayIndexOutOfBoundsException
+            //int x = a[4] / a[5]; ArithmeticException
+        }catch (ArrayIndexOutOfBoundsException aeExc){
+            System.out.println("An array index out of bound");
+        }catch (ArithmeticException arExc){
+            System.out.println("Not divisible by an index out of bound");
+        }catch (Exception exc){
+            System.out.println("error detected");
+        }
+        System.out.println("end of program");
+    }
+    public static int[] anArray(int...anArray){
+        return anArray;
+    }
+}
