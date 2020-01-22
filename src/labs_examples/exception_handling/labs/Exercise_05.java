83f77e3209a2ba14bfd71af16a0ee6ae23b1f6df
nngocjade <nngocjade@gmail.com>
2020-01-22 16:12:28 -0600

    done

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -6,3 +6,22 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate how to throw an exception.
  *
  */
+class Exercise_5{
+    public static void main(String[] args) throws Exception {
+        try{
+            divide(4, 0);
+        }catch(ArithmeticException arExc){
+            System.out.println("not divisible by 0");
+        }
+    }
+    public static void divide(int a, int b)throws Exception{
+        try {
+            int x = a / b;
+            System.out.println(a + " / " + b + " = " + x);
+        }catch (Exception e){
+            System.out.println("Error detected");
+            throw e;
+        }
+
+    }
+}
