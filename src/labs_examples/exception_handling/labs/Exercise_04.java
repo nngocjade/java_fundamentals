98d90460bcf60ea4a0d18c8683b51f7bd9efdde1
nngocjade <nngocjade@gmail.com>
2020-01-22 11:18:26 -0600

    done

--- src/labs_examples/exception_handling/labs/Exercise_04.java
+++ src/labs_examples/exception_handling/labs/Exercise_04.java
@@ -6,3 +6,24 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a nested try/catch.
  *
  */
+class Exercise_4{
+    public static void main(String[] args) {
+
+        int[] list = {33,21,22,55,44};
+
+        try{//Outer block exception
+            int z = list[5];
+            System.out.println(z);
+        }catch(Exception e){
+            try{//inner block exception
+                int x = list[0] / list[18];
+                //int x = list[0] / list[10];//inner block error
+            }catch (IndexOutOfBoundsException aeExc){
+                System.out.println("Inner block error: Index out of bound");
+            }
+            System.out.println("Outer block error detected");
+        }
+        System.out.println("End of program");
+    }
+
+}
