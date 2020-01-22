2486dd3b2e403a882ab6efbfe4465eec58f81e8e
nngocjade <nngocjade@gmail.com>
2020-01-21 23:30:14 -0600

    in progress

--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,22 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+
+class Exercise_3{
+    public static void main(String[] args) {
+
+        boolean t = trueOrFalse(true);
+
+        try{
+            t = false;
+        }catch (ArithmeticException aeExc){
+            System.out.println("boolean value is not false");
+        }finally {
+            System.out.println("boolean value will always be true");
+        }
+
+    }
+    public static boolean trueOrFalse(boolean b){
+        return b;
+    }
+}
