3a0654b681f0e2a7fdd4a4bbebd0bf66b5b0ce6f
nngocjade <nngocjade@gmail.com>
2020-01-22 18:02:10 -0600

    done

--- src/labs_examples/exception_handling/labs/Exercise_06.java
+++ src/labs_examples/exception_handling/labs/Exercise_06.java
@@ -7,3 +7,21 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+class Exercise_6 {
+    public static void main(String[] args) throws Exception {
+
+        try {
+            int x = anArray(12);
+            System.out.println(x);
+        } catch (IndexOutOfBoundsException aeExc) {
+            System.out.println("Error: index out of bound");
+        }
+        System.out.println("end of program");
+    }
+
+    public static int anArray(int i) throws Exception {
+        int[] a = {2, 3, 4, 5, 67, 8};
+
+        return a[i];
+    }
+}
