9627329306b2af41b199e6a1cb0a6a2f5d1e93f2
nngocjade <nngocjade@gmail.com>
2020-01-24 00:25:20 -0600

    done

--- src/labs_examples/exception_handling/labs/Exercise_07.java
+++ src/labs_examples/exception_handling/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.exception_handling.labs;
 
+import java.util.Scanner;
+
 /**
  * Exception Handling Exercise 7:
  *
@@ -7,18 +9,34 @@ package labs_examples.exception_handling.labs;
  *      2) Demonstrate a method throwing your custom exception.
  */
 class Exercise_7{
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws SalonFullException {
+       try{
+           addCustomerToSalon();
+       }catch (SalonFullException e){
+           System.out.println(e.toString());
+       }
     }
-    public static int addCustomerToSalon() throws SalonFullException {
+    public static void addCustomerToSalon() throws SalonFullException {
+        Scanner scanner = new Scanner(System.in);
 
-    }
+        System.out.println("Hello, how can I help you?");
+        String response = scanner.nextLine();
 
-}
-class Salon{
-    private int seats;
-
-}
-class SalonFullException extends Exception{
+        if (response.equalsIgnoreCase("I would like to get in please")){
+            throw new SalonFullException();
 
         }
+        System.out.println("Would you like to come back at another time?");
+        String response1 = scanner.next();
+
+        System.out.println("Great! You are schedule!");
+    }
+
+}
+
+class SalonFullException extends Exception{
+    @Override
+    public String toString() {
+        return "SalonFullException{Sorry, we are currently full}";
+    }
+}
