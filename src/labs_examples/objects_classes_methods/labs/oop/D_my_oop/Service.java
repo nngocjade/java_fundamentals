a9cdd458c45108513d37fb329ca968cac8acf760
nngocjade <nngocjade@gmail.com>
2020-01-25 23:36:37 -0600

    in progress

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/Service.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/Service.java
@@ -1,4 +1,22 @@
 package labs_examples.objects_classes_methods.labs.oop.D_my_oop;
 
-public class Services {
+public class Service {
+
+    //TODO: ask Ryan, would it be better to create a class and detail all the different types of services or create just one instance variable called service?
+
+    /*services include:
+    Manicure
+    pedicure
+    manicure + pedicure
+    polish change
+    acrylic
+    fill
+    hard gel
+    maintenance
+    deluxe Manicure
+    deluxe Pedicure
+    art/design
+    dip powder
+    */
+
 }
