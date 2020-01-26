285bd5942b602ba55cef824f34f2358ced09e04f
nngocjade <nngocjade@gmail.com>
2020-01-25 23:36:09 -0600

    in progress

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/BookingSystem.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/BookingSystem.java
@@ -2,4 +2,14 @@ package labs_examples.objects_classes_methods.labs.oop.D_my_oop;
 
 //superclass
 public class BookingSystem {
+
+    private int time;
+    private int date;
+    private double businessHour;
+
+    private static int person;
+    private String firstName;
+    private String LastName;
+
+    private int group;
 }
