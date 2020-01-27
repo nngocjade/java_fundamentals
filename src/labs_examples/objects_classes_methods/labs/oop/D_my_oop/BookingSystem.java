30944bdafb2a5ec3566d9a0aefc39ba6793e1fb2
nngocjade <nngocjade@gmail.com>
2020-01-27 10:18:13 -0800

    in progress

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/BookingSystem.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/BookingSystem.java
@@ -9,7 +9,39 @@ public class BookingSystem {
 
     private static int person;
     private String firstName;
-    private String LastName;
+    private String lastName;
+    private int phoneNum;
+    private String email;
 
     private int group;
+
+    private String service;
+
+    public BookingSystem(){
+    }
+
+    public BookingSystem(int time, int date, double businessHour) {
+        this.time = time;
+        this.date = date;
+        this.businessHour = businessHour;
+    }
+
+    public BookingSystem(String firstName, String lastName, int phoneNum, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+        this.email = email;
+    }
+
+    public BookingSystem(int time, int date, double businessHour, String firstName, String lastName, int phoneNum, String email, int group, String service) {
+        this.time = time;
+        this.date = date;
+        this.businessHour = businessHour;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+        this.email = email;
+        this.group = group;
+        this.service = service;
+    }
 }
