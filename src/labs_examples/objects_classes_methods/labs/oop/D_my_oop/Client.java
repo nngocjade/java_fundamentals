f5c372f92e85efff119d8e7adce39d73ffb9d8f7
nngocjade <nngocjade@gmail.com>
2020-01-27 10:24:06 -0800

    in progress

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/Client.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/Client.java
@@ -5,4 +5,18 @@ public class Client extends BookingSystem {
     private String interest;
     private String style;
     private String request;
+
+    public Client(int time, int date) {
+        super(time, date);
+    }
+
+    public Client(String interest, String style, String request) {
+        this.interest = interest;
+        this.style = style;
+        this.request = request;
+    }
+
+    public Client(String firstName, String lastName, int phoneNum, String email) {
+        super(firstName, lastName, phoneNum, email);
+    }
 }
--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/NailTechnician.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/NailTechnician.java
@@ -4,4 +4,21 @@ package labs_examples.objects_classes_methods.labs.oop.D_my_oop;
 public class NailTechnician extends BookingSystem {
     private String skill;
 
+    public NailTechnician(String skill) {
+        this.skill = skill;
+    }
+
+    public NailTechnician(int time, int date) {
+        super(time, date);
+
+    }
+
+    public NailTechnician(int time, int date, double businessHour) {
+        super(time, date, businessHour);
+    }
+
+    public NailTechnician(String firstName, String lastName, int phoneNum, String email) {
+        super(firstName, lastName, phoneNum, email);
+
+    }
 }
