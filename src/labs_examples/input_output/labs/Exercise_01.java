d6fe9665d1e987017d1646a4b9138e360e7f32c2
nngocjade <nngocjade@gmail.com>
2020-01-29 09:55:22 +0700

    in progress

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,8 @@
 package labs_examples.input_output.labs;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -12,6 +15,22 @@ package labs_examples.input_output.labs;
 class Example {
     public static void main(String[] args) {
 
+        FileInputStream inputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+
+        try{
+            inputStream = new FileInputStream();
+            bufferedInputStream = new BufferedInputStream(inputStream);
+            byte[] buffer = new byte[5];
+            int bytesRead = 0;
+            // this while loop will read the file 5 bytes at a time
+            while ((bytesRead = bufferedInputStream.read(buffer)) != -1){
+                // print out bytes read to console
+                System.out.print(new String(buffer, 0, bytesRead));
+            }
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        } finally {
 
 
     }
