11e35a814d8dcd656bb7bfc6a17ed0a04d71881e
nngocjade <nngocjade@gmail.com>
2020-01-30 02:25:01 +0700

    in progress

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -15,52 +15,55 @@ import java.nio.Buffer;
 class Example {
     public static void main(String[] args) throws IOException {
 
-        String fileReadPath = "src/labs_examples/input_output/files/char_data_copy.txt";
-        String fileWritePath = "src/labs_examples/input_output/files/char_data_rewrite.txt";
-
-        FileInputStream fin = null;
-        BufferedInputStream bufferedInputStream = null;
-        FileOutputStream fout = null;
-        BufferedOutputStream bufferedOutputStream = null;
-
-        //Copy a File.
-        try{
-            //Attempt to open the files
-            fin = new FileInputStream(fileReadPath);
-            bufferedInputStream = new BufferedInputStream(fin);
-            fout = new FileOutputStream(fileWritePath);
-            bufferedOutputStream = new BufferedOutputStream(fout);
-
-
-            //holds collection of bytes to read at once
+        BufferedInputStream fin = new BufferedInputStream(new FileInputStream("src/labs_examples/input_output/files/char_data_copy.txt"));
+        BufferedOutputStream fout = new BufferedOutputStream(new FileOutputStream("src/labs_examples/input_output/files/char_data_rewrite.txt"));
+        int i;
+        do {
+            //TODO ask Ryan:
             byte[] buffer = new byte[5];
-            int bytesRead = 0;
-            // this while loop will read the file 5 bytes at a time
-            while ((bytesRead = bufferedInputStream.read(buffer)) != -1){
-                // print out bytes read to console
-                bufferedOutputStream.write(bytesRead);
-            }
-        } catch (IOException exc) {
-            exc.printStackTrace();
-        } finally {
-            try {
-                fin.close();
-                bufferedInputStream.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
+            i = fin.read(buffer);
+            if (i != -1)
+                fout.write(i);
+        } while (i != -1);
+        fin.close();
+        fout.close();
     }
-}
 
-//    BufferedInputStream fin = new BufferedInputStream(new FileInputStream("in.dat"));
-//    BufferedOutputStream fout = new BufferedOutputStream(new FileOutputStream("out.dat"));
-//    int i;
-//    do {
-//            i = fin.read();
-//            if (i != -1)
-//            fout.write(i);
-//            } while (i != -1);
-//            fin.close();
-//            fout.close();
+//        String fileReadPath = "src/labs_examples/input_output/files/char_data_copy.txt";
+//        String fileWritePath = "src/labs_examples/input_output/files/char_data_rewrite.txt";
+//
+//        FileInputStream fin = null;
+//        BufferedInputStream bufferedInputStream = null;
+//        FileOutputStream fout = null;
+//        BufferedOutputStream bufferedOutputStream = null;
+//
+//        //Copy a File.
+//        try{
+//            //Attempt to open the files
+//            fin = new FileInputStream(fileReadPath);
+//            bufferedInputStream = new BufferedInputStream(fin);
+//            fout = new FileOutputStream(fileWritePath);
+//            bufferedOutputStream = new BufferedOutputStream(fout);
+//
+//
+//            //holds collection of bytes to read at once
+//            byte[] buffer = new byte[5];
+//            int bytesRead = 0;
+//            // this while loop will read the file 5 bytes at a time
+//            while ((bytesRead = bufferedInputStream.read(buffer)) != -1){
+//                // print out bytes read to console
+//                bufferedOutputStream.write(bytesRead);
 //            }
+//        } catch (IOException exc) {
+//            exc.printStackTrace();
+//        } finally {
+//            try {
+//                fin.close();
+//                bufferedInputStream.close();
+//            } catch (IOException ex) {
+//                ex.printStackTrace();
+//            }
+//        }
+    }
+
+
