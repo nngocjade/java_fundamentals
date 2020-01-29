5bca8f4a5a8d37b651ad1a0e5570a9a5fbca513b
nngocjade <nngocjade@gmail.com>
2020-01-30 04:01:03 +0700

    done for now

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -14,67 +14,68 @@ import java.nio.Buffer;
 
 class Example {
     public static void main(String[] args) throws IOException {
-
-//        BufferedInputStream fin = new BufferedInputStream(new FileInputStream("src/labs_examples/input_output/files/char_data_copy.txt"));
-//        BufferedOutputStream fout = new BufferedOutputStream(new FileOutputStream("src/labs_examples/input_output/files/char_data_rewrite.txt"));
-//        int i;
-//        do {
-//            //TODO ask Ryan:
-//            byte[] buffer = new byte[5];
-//            i = fin.read(buffer);
-//            if (i != -1)
-//                fout.write(i);
-//        } while (i != -1);
-//        fin.close();
-//        fout.close();
-//    }
-
-        String fileReadPath = "src/labs_examples/input_output/files/char_data_copy.txt";
-        String fileWritePath = "src/labs_examples/input_output/files/char_data_rewrite.txt";
-
-        FileInputStream fin = null;
-        BufferedInputStream bufferedInputStream = null;
-        FileOutputStream fout = null;
-        BufferedOutputStream bufferedOutputStream = null;
-
-        //Copy a File.
+        //TODO ask ryan:
+        BufferedInputStream fin = new BufferedInputStream(new FileInputStream("src/labs_examples/input_output/files/char_data_copy.txt"));
+        BufferedOutputStream fout = new BufferedOutputStream(new FileOutputStream("src/labs_examples/input_output/files/char_data_rewrite.txt"));
+        int i;
         try {
-            //Attempt to open the files
-            fin = new FileInputStream(fileReadPath);
-            bufferedInputStream = new BufferedInputStream(fin);
-            fout = new FileOutputStream(fileWritePath);
-            bufferedOutputStream = new BufferedOutputStream(fout);
-
-
-            //holds collection of bytes to read at once
             byte[] buffer = new byte[5];
-            int bytesRead = 0;
-            // this while loop will read the file 5 bytes at a time
-            while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
-
-                bufferedOutputStream.write((char) bytesRead);
+            while (fin.read(buffer) != -1) {
+                fout.write(buffer);
             }
-        } catch (IOException exc) {
-            exc.printStackTrace();
-        } finally {
-            try {
-                if(fin != null)
-                fin.close();
-                if(bufferedInputStream != null)
-                bufferedInputStream.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-            try {
-                if(fout != null)
-                    fout.close();
-                if(bufferedOutputStream != null)
-                bufferedOutputStream.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+        }catch(IOException ex) {
+            ex.printStackTrace();
         }
+        fin.close();
+        fout.close();
     }
-}
+
+//        String fileReadPath = "src/labs_examples/input_output/files/char_data_copy.txt";
+//        String fileWritePath = "src/labs_examples/input_output/files/char_data_rewrite.txt";
+//
+//        FileInputStream fin = null;
+//        BufferedInputStream bufferedInputStream = null;
+//        FileOutputStream fout = null;
+//        BufferedOutputStream bufferedOutputStream = null;
+//
+//        //Copy a File.
+//        try {
+//            //Attempt to open the files
+//            fin = new FileInputStream(fileReadPath);
+//            bufferedInputStream = new BufferedInputStream(fin);
+//            fout = new FileOutputStream(fileWritePath);
+//            bufferedOutputStream = new BufferedOutputStream(fout);
+//
+//
+//            //holds collection of bytes to read at once
+//            byte[] buffer = new byte[5];
+//            int bytesRead = 0;
+//            // this while loop will read the file 5 bytes at a time
+//            while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+//
+//                bufferedOutputStream.write((char) bytesRead);
+//            }
+//        } catch (IOException exc) {
+//            exc.printStackTrace();
+//        } finally {
+//            try {
+//                if(fin != null)
+//                fin.close();
+//                if(bufferedInputStream != null)
+//                bufferedInputStream.close();
+//            } catch (IOException ex) {
+//                ex.printStackTrace();
+//            }
+//            try {
+//                if(fout != null)
+//                    fout.close();
+//                if(bufferedOutputStream != null)
+//                bufferedOutputStream.close();
+//            } catch (IOException ex) {
+//                ex.printStackTrace();
+//            }
+//        }
+    }
+
 
 
