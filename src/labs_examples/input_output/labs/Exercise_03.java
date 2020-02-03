02e3a1aed0d109e3df0119c7839849f2097eb873
nngocjade <nngocjade@gmail.com>
2020-02-03 18:27:55 +0700

    in progress

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -1,5 +1,9 @@
 package labs_examples.input_output.labs;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Input/Output Exercise 3: variety
  *
@@ -10,11 +14,42 @@ package labs_examples.input_output.labs;
  *
  */
 class Exercise_3{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String fileReadPath = "";
+        String fileWritePath = "";
+
+
 
     }
-    public static void byteByByte(){
+    public static void byteByByte(String fileReadPath, String fileWritePath) throws IOException {
+        // declare Input and Output Streams outside of Try statement so they're
+        // accessible in the Finally statement
+        FileInputStream in = null;
+        FileOutputStream out = null;
 
+        try {
+            // initialize them within the Try in case the files do not exist or cannot be accessed
+            in = new FileInputStream(fileReadPath);
+            out = new FileOutputStream(fileWritePath);
+            int c;
+
+            // while there is more data to read in the InputStream "in"
+            // InputStream's read() method returns -1 when the end of the file is reached
+            while ((c = in.read()) != -1) {
+                // write the data (byte by byte) to the OutputStream "out"
+                out.write(c);
+            }
+        } catch (IOException exc){
+            System.out.println("An error occurred: " + exc.getMessage());
+        } finally {
+            // close connections to files
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
     }
     //with buffer
     public static void byte2(){
