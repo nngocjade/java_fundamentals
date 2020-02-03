bd82c2ddc5e1ddc256f1deda66f4fde2e47b6a48
nngocjade <nngocjade@gmail.com>
2020-02-03 18:38:35 +0700

    in progress

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -15,29 +15,25 @@ import java.io.IOException;
  */
 class Exercise_3{
     public static void main(String[] args) throws IOException {
-        String fileReadPath = "";
-        String fileWritePath = "";
+        String fileReadPath = "src/labs_examples/input_output/files/byte_data";
 
-
+        byteByByte(fileReadPath);
 
     }
-    public static void byteByByte(String fileReadPath, String fileWritePath) throws IOException {
+    public static void byteByByte(String fileReadPath) throws IOException {
         // declare Input and Output Streams outside of Try statement so they're
         // accessible in the Finally statement
         FileInputStream in = null;
-        FileOutputStream out = null;
 
         try {
             // initialize them within the Try in case the files do not exist or cannot be accessed
             in = new FileInputStream(fileReadPath);
-            out = new FileOutputStream(fileWritePath);
             int c;
 
-            // while there is more data to read in the InputStream "in"
             // InputStream's read() method returns -1 when the end of the file is reached
             while ((c = in.read()) != -1) {
-                // write the data (byte by byte) to the OutputStream "out"
-                out.write(c);
+                // write the data (byte by byte) to console
+                System.out.print((char) c);
             }
         } catch (IOException exc){
             System.out.println("An error occurred: " + exc.getMessage());
@@ -45,9 +41,6 @@ class Exercise_3{
             // close connections to files
             if (in != null) {
                 in.close();
-            }
-            if (out != null) {
-                out.close();
             }
         }
     }
