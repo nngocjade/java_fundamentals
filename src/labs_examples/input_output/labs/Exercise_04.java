972549779e0ac2da0d3a2f80e621147b1aaefa7d
nngocjade <nngocjade@gmail.com>
2020-02-06 15:27:14 +0700

    in progress

--- src/labs_examples/input_output/labs/Exercise_04.java
+++ src/labs_examples/input_output/labs/Exercise_04.java
@@ -12,3 +12,58 @@ package labs_examples.input_output.labs;
  *      Then, write the arraylist of objects back out to a new .csv file. Ensure that the resulting csv file is valid.
  *
  */
+
+class Exercise_4{
+    public static void main(String[] args) {
+
+    }
+}
+
+//POJO
+class Elephant{
+    private String name;
+    private String personality;
+    private int age;
+    private double weight;
+
+    public Elephant(String name, String personality, int age, double weight) {
+        this.name = name;
+        this.personality = personality;
+        this.age = age;
+        this.weight = weight;
+    }
+    public Elephant(){
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPersonality() {
+        return personality;
+    }
+
+    public void setPersonality(String personality) {
+        this.personality = personality;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
