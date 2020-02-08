a516630a25f7597b4d2dbc7e84b103fda39cfa61
nngocjade <nngocjade@gmail.com>
2020-02-08 23:43:29 +0700

    in progress

--- src/labs_examples/multi_threading/labs/Exercise_01.java
+++ src/labs_examples/multi_threading/labs/Exercise_01.java
@@ -9,18 +9,37 @@ package labs_examples.multi_threading.labs;
  */
 
 class Exercise_1{
+    public static void main(String[] args) {
+        System.out.println("Printing and Starting with Main thread");
+        //Option 1
+        Runnable1 runnable1 = new Runnable1();
+
+
+        //Option 2
+    }
 
 }
 class Runnable1 implements Runnable{
 
     String threadName;
-
     Runnable1(String name){
         threadName = name;
+    }
+
+    public Runnable1() {
+
     }
 
     public void run() {
         System.out.println("Starting" + threadName);
 
+        for(int count=0; count<5; count++){
+            try {
+                Thread.sleep(1000);
+                System.out.println("In" + threadName + "count is" + count);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
