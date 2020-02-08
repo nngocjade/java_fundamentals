f7c5c2ff93e45067e0acea54cd74d0da4b8a581e
nngocjade <nngocjade@gmail.com>
2020-02-08 23:59:51 +0700

    in progress

--- src/labs_examples/multi_threading/labs/Exercise_01.java
+++ src/labs_examples/multi_threading/labs/Exercise_01.java
@@ -12,11 +12,16 @@ class Exercise_1{
     public static void main(String[] args) {
         System.out.println("Printing and Starting with Main thread");
         //Option 1
+        //Creating runnable object
         Runnable1 runnableOne = new Runnable1();
-        Thread threadOne = new Thread(runnableOne, "Option 1");
+        //passing runnableOne and Thread name of "ThreadOption1"
+        Thread threadOne = new Thread(runnableOne, "ThreadOption1");
+        //starting thread
         threadOne.start();
 
         //Option 2
+
+
     }
 
 }
@@ -26,12 +31,12 @@ class Runnable1 implements Runnable{
 
     @Override
     public void run() {
-        System.out.println("Starting" + Thread.currentThread().getName());
+        System.out.println("Starting " + Thread.currentThread().getName());
 
         for(int count=0; count<5; count++){
             try {
                 Thread.sleep(1000);
-                System.out.println("In" + Thread.currentThread().getName() + "count is" + count);
+                System.out.println("In " + Thread.currentThread().getName() + " count is " + count);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
