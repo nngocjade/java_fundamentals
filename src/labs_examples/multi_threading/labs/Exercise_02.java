ec9fa257def4a3221049a4e773a75d9a2e4cb3b6
nngocjade <nngocjade@gmail.com>
2020-02-09 16:19:35 +0700

    in progress

--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -1,7 +1,24 @@
 package labs_examples.multi_threading.labs;
 
+import java.sql.SQLOutput;
+
 /**
  * Multithreading Exercise 2:
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+class Exercise_2{
+    public static void main(String[] args) {
+        System.out.println("Let's multiple simultaneously");
+
+    }
+}
+//POJO
+class Culture implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("");
+    }
+}
