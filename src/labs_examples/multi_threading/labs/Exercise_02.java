ccfcf644982ce6cd0a7144c46cb240b9e528bcc1
nngocjade <nngocjade@gmail.com>
2020-02-09 17:01:22 +0700

    done

--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -1,5 +1,6 @@
 package labs_examples.multi_threading.labs;
 
+import java.io.IOException;
 import java.sql.SQLOutput;
 
 /**
@@ -12,13 +13,43 @@ class Exercise_2{
     public static void main(String[] args) {
         System.out.println("Let's multiple simultaneously");
 
+        MultiplyingSimultaneously multiplyingSimultaneously = new MultiplyingSimultaneously(2);
+        //passing runnableOne and Thread name of "ThreadOption1"
+        Thread threadMultiplyOne = new Thread(multiplyingSimultaneously, "ThreadMultiplyOne");
+        //starting thread
+        threadMultiplyOne.start();
+
     }
 }
 //POJO
-class Culture implements Runnable{
+class MultiplyingSimultaneously implements Runnable{
+
+    private int y;
+
+    public MultiplyingSimultaneously(int y) {
+        this.y = y;
+    }
 
     @Override
     public void run() {
-        System.out.println("");
+        System.out.println("Starting multiply1");
+
+        //multiply
+
+        int total;
+        //as x increments by 1, gets multiplied by y = 2 (every incremented value of x is multiplied by y = 2)
+        //x @ 0 times y @ 2 = 0
+        // x @ 1 times y @ 2 = 2
+        //x @ 2 times y @ 2 = 4
+        // and so on..
+        for(int x=0; x<20; x++){
+            try{
+                total = x*y;
+                System.out.println("In " + Thread.currentThread().getName() + " total = " + total);
+            }catch (Exception e){
+                System.out.println("error detected");
+            }
+
+        }
     }
 }
