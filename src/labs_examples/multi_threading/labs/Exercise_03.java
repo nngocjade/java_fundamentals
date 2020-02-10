7f7fc013c02cde00f5f363580588756ef37da7a1
nngocjade <nngocjade@gmail.com>
2020-02-10 19:15:21 +0700

    in progress

--- src/labs_examples/multi_threading/labs/Exercise_03.java
+++ src/labs_examples/multi_threading/labs/Exercise_03.java
@@ -11,18 +11,8 @@ class Exercise_3{
         System.out.println("Let's multiple simultaneously");
 
         //prioritizing threads in Exercise 2
-        MultiplyingSimultaneously multiplyingSimultaneously = new MultiplyingSimultaneously(2);
-        //passing runnableOne and Thread name of "ThreadOption1"
-        Thread threadMultiplyOne = new Thread(multiplyingSimultaneously, "ThreadMultiply1");
-        //starting thread
-        threadMultiplyOne.start();
-
-        MultiplyingSimultaneously multiplyingSimultaneously1 = new MultiplyingSimultaneously(3);
-        //passing runnableOne and Thread name of "ThreadOption1"
-        Thread threadMultiplyTwo = new Thread(multiplyingSimultaneously1, "ThreadMultiply2");
-        //starting thread
-        threadMultiplyTwo.start();
-
+        MultiplyingSimultaneously multiplyingSimultaneously = new MultiplyingSimultaneously(2, "ThreadMultiply2");
+        MultiplyingSimultaneously multiplyingSimultaneously1 = new MultiplyingSimultaneously(3, "ThreadMultiply3");
 
     }
 }
