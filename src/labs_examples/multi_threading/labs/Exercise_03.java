5438357757150da87c29b3deb4392b13de4ef0ad
nngocjade <nngocjade@gmail.com>
2020-02-11 01:43:43 +0700

    in progress

--- src/labs_examples/multi_threading/labs/Exercise_03.java
+++ src/labs_examples/multi_threading/labs/Exercise_03.java
@@ -7,12 +7,13 @@ package labs_examples.multi_threading.labs;
  */
 
 class Exercise_3{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Let's multiple simultaneously");
 
         //prioritizing threads in Exercise 2
         MultiplyingSimultaneously multiplyingSimultaneously = new MultiplyingSimultaneously(2, "ThreadMultiply2");
+        multiplyingSimultaneously.setPriority(10);
         MultiplyingSimultaneously multiplyingSimultaneously1 = new MultiplyingSimultaneously(3, "ThreadMultiply3");
-
+        multiplyingSimultaneously1.setPriority(1);
     }
 }
