036e48ecf42b023066d381b96cc9fe0fbf97e432
nngocjade <nngocjade@gmail.com>
2020-02-12 19:57:13 +0700

    in progress

--- src/labs_examples/multi_threading/labs/Exercise_05.java
+++ src/labs_examples/multi_threading/labs/Exercise_05.java
@@ -5,3 +5,26 @@ package labs_examples.multi_threading.labs;
  *
  *      Demonstrate the use of a wait() and notify()
  */
+
+//alarm goes off
+class AlarmClock{
+    private int hour;
+
+    synchronized void ring(int hour){
+        this.hour = hour;
+
+        for (int t = 1; t < 24; t++){
+            if(t == 5){
+                System.out.println("Alarm goes off when: t = " + t);
+            }
+        }
+    }
+
+}
+
+//controller
+class Exercise_5{
+    public static void main(String[] args) {
+
+    }
+}
