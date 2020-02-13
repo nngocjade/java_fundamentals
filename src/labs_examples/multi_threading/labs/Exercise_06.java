52a3749c70d25ead5de257425a8e215eef950a21
nngocjade <nngocjade@gmail.com>
2020-02-13 23:36:46 +0700

    done

--- src/labs_examples/multi_threading/labs/Exercise_06.java
+++ src/labs_examples/multi_threading/labs/Exercise_06.java
@@ -9,50 +9,7 @@ package labs_examples.multi_threading.labs;
 
 //OddThread to print odd numbers
 //Calls printOdd() method of SharedPrinter class until limit is exceeded.
-class OddThread extends Thread
-{
-    int limit;
-    sharedPrinter printer;
-    public OddThread(int limit, sharedPrinter printer)
-    {
-        this.limit = limit;
-        this.printer = printer;
-    }
-    @Override
-    public void run()
-    {
-        int oddNumber = 1;        //First odd number is 1
-        while (oddNumber <= limit)
-        {
-            printer.printOdd(oddNumber);       //Calling printOdd() method of SharedPrinter class
-            oddNumber = oddNumber + 2;         //Incrementing to next odd number
-        }
-    }
-}
-//EvenThread to print even numbers.
-//Calls printEven() method of SharedPrinter class until limit is exceeded.
-class EvenThread extends Thread
-{
-    int limit;
-    sharedPrinter printer;
-    public EvenThread(int limit, sharedPrinter printer)
-    {
-        this.limit = limit;
-        this.printer = printer;
-    }
-    @Override
-    public void run()
-    {
-        int evenNumber = 2;           //First even number is 2
-        while (evenNumber <= limit)
-        {
-            printer.printEven(evenNumber);          //Calling printEven() method of SharedPrinter class
-            evenNumber = evenNumber + 2;           //Incrementing to next even number
-        }
-    }
-}
-class sharedPrinter
-{
+class sharedPrinter {
     //A boolean flag variable to check whether odd number is printed or not
     //Initially it is false.
     boolean isOddPrinted = false;
@@ -61,27 +18,21 @@ class sharedPrinter
     //if isOddPrinted is true then it waits until next even number is printed by EvenThread
     //If isOddPrinted is false then prints next odd number, sets isOddPrinted to true
     //sleeps for 1 second before notifying EvenThread
-    synchronized void printOdd(int number)
-    {
-        while (isOddPrinted)
-        {
-            try
-            {
+    synchronized void printOdd(int number) {
+        while (isOddPrinted) {
+            try {
                 wait();
             }
-            catch (InterruptedException e)
-            {
+            catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
         System.out.println(Thread.currentThread().getName()+" : "+number);
         isOddPrinted = true;
-        try
-        {
-            Thread.sleep(1000);
+        try {
+            Thread.sleep(100);
         }
-        catch (InterruptedException e)
-        {
+        catch (InterruptedException e) {
             e.printStackTrace();
         }
         notify();
@@ -91,32 +42,61 @@ class sharedPrinter
     //if isOddPrinted is false then it waits until next odd number is printed by OddThread
     //If isOddPrinted is true then it prints next even number, sets isOddPrinted to false
     //sleeps for 1 second before notifying OddThread
-    synchronized void printEven(int number)
-    {
-        while (! isOddPrinted)
-        {
-            try
-            {
+    synchronized void printEven(int number) {
+        while (! isOddPrinted) {
+            try {
                 wait();
             }
-            catch (InterruptedException e)
-            {
+            catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
         System.out.println(Thread.currentThread().getName()+" : "+number);
         isOddPrinted = false;
-        try
-        {
-            Thread.sleep(1);
+        try {
+            Thread.sleep(100);
         }
-        catch (InterruptedException e)
-        {
+        catch (InterruptedException e) {
             e.printStackTrace();
         }
         notify();
     }
 }
+class OddThread extends Thread {
+    int limit;
+    sharedPrinter printer;
+    public OddThread(int limit, sharedPrinter printer) {
+        this.limit = limit;
+        this.printer = printer;
+    }
+    @Override
+    public void run() {
+        int oddNumber = 1;        //First odd number is 1
+        while (oddNumber <= limit) {
+            printer.printOdd(oddNumber);       //Calling printOdd() method of SharedPrinter class
+            oddNumber = oddNumber + 2;         //Incrementing to next odd number
+        }
+    }
+}
+//EvenThread to print even numbers.
+//Calls printEven() method of SharedPrinter class until limit is exceeded.
+class EvenThread extends Thread {
+    int limit;
+    sharedPrinter printer;
+    public EvenThread(int limit, sharedPrinter printer) {
+        this.limit = limit;
+        this.printer = printer;
+    }
+    @Override
+    public void run() {
+        int evenNumber = 2;           //First even number is 2
+        while (evenNumber <= limit) {
+            printer.printEven(evenNumber);          //Calling printEven() method of SharedPrinter class
+            evenNumber = evenNumber + 2;           //Incrementing to next even number
+        }
+    }
+}
+
 //Main Class
 class Exercise_6{
     public static void main(String[] args){
