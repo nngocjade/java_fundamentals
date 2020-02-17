1e241ca54526a27003f7bcd347f01ba84a8f8161
nngocjade <nngocjade@gmail.com>
2020-02-17 19:34:42 +0700

    in progress

--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -1,8 +1,5 @@
 package labs_examples.generics.labs;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 /**
  * Generics Exercise 3:
  *
@@ -20,5 +17,21 @@ import java.util.Collection;
  */
 
 public class Exercise_03 {
-    
+    public static void main(String[] args) {
+
+    }
+//     1) Write a generic method that accepts two generic arguments. This generic method should only accept
+//     arguments which are sublasses of Number. The generic method must return the sum (as a double) of whatever two
+//     numbers were passed in regardless of their type.
+    public static <N> double sum (N a, N b) {
+        N d = a + b;
+        return d;
+
+    }
+
+//     2) Write a generic method to count the number of elements in a collection of Strings that are palindromes
+
+//     3) Write a generic method to exchange the positions of two different elements in an array.
+
+//     4) Write a generic method to find the largest element within the range (begin, end) of a list.
 }
