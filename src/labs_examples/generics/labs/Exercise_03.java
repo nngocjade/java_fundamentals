e06468573ac8a628bc8b049897a1c8169b4b11bc
nngocjade <nngocjade@gmail.com>
2020-02-18 14:55:28 +0700

    in progress

--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -1,5 +1,8 @@
 package labs_examples.generics.labs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Generics Exercise 3:
  *
@@ -18,16 +21,19 @@ package labs_examples.generics.labs;
 
 public class Exercise_03 {
     public static void main(String[] args) {
+        System.out.println(add(4.5,5.6));
+
 
     }
+
 //     1) Write a generic method that accepts two generic arguments. This generic method should only accept
 //     arguments which are sublasses of Number. The generic method must return the sum (as a double) of whatever two
 //     numbers were passed in regardless of their type.
-    public static <N> double sum (N a, N b) {
-        N d = a + b;
-        return d;
-
-    }
+    public static<T extends Number> T add(T x, T y){
+    Double sum;
+    sum = x.doubleValue() + y.doubleValue();
+    return (T) sum;
+}
 
 //     2) Write a generic method to count the number of elements in a collection of Strings that are palindromes
 
@@ -35,3 +41,14 @@ public class Exercise_03 {
 
 //     4) Write a generic method to find the largest element within the range (begin, end) of a list.
 }
+
+//    //  Inside generic, create a method called printArray()
+//    public static <T> List<T> printArray(T... elements){
+//        List<T> lst = new ArrayList<>();
+//        for(T element: elements) {
+//            lst.add(element);
+//        }
+//        return lst;
+//
+////  printArray must print the contents of an array that receives as a parameter
+//    }
