b42e90c915f83ae6408f53c172320db747658438
nngocjade <nngocjade@gmail.com>
2020-02-22 18:17:21 +0700

    in progress

--- src/labs_examples/lambdas/labs/Exercise_01.java
+++ src/labs_examples/lambdas/labs/Exercise_01.java
@@ -29,6 +29,10 @@ class Exercise_1 {
 
         //      2) Implement the previous functional interface with a lambda expression and use it. Also demonstrate creating
 //        *          an anonymous inner class from this interface.
+        NoParameterInterface noParameterInterface = Exercise_1::noParameter;
+
+        //        *      6) Implement the previous functional interface with a lambda expression and use it. Also demonstrate creating
+//        *          an anonymous inner class from this interface.
         MathInterface obj = (a, b) -> {
             int m = a / b;
             return m;
@@ -38,24 +42,28 @@ class Exercise_1 {
         System.out.println();
 
     }
+
+    private static void noParameter() {
+    }
 }
 
 //               1) Demonstrate creating a functional interface with an abstract method that takes no parameters and returns void
-interface MathInterface {
-    int divide(int a, int b);
+interface NoParameterInterface{
+    void noParameter();
 }
-
-//        *
 
 //        *      3) Demonstrate creating a functional interface with an abstract method that takes 1 parameter and returns a
 //        *          value of the same type as the parameter
+
 //        *      4) Implement the previous functional interface with a lambda expression and use it. Also demonstrate creating
 //        *          an anonymous inner class from this interface.
 //        *
 //        *      5) Demonstrate creating a functional interface with an abstract method that takes 2 parameters and returns a
 //        *          value
-//        *      6) Implement the previous functional interface with a lambda expression and use it. Also demonstrate creating
-//        *          an anonymous inner class from this interface.
+interface MathInterface {
+    int divide(int a, int b);
+}
+
 //        *
 //        *      7) Demonstrate the use of at least two built-in functional interfaces from the java.util.function package.
 //        *
