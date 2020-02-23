ee00c7e86f4e56e50eb985fff374b51c345e7654
nngocjade <nngocjade@gmail.com>
2020-02-23 20:17:24 +0700

    in progress

--- src/labs_examples/lambdas/labs/Exercise_02.java
+++ src/labs_examples/lambdas/labs/Exercise_02.java
@@ -1,5 +1,8 @@
 package labs_examples.lambdas.labs;
 
+import java.util.function.DoubleToIntFunction;
+import java.util.function.Function;
+
 /**
  * Lambdas Exercise 2:
  *
@@ -7,3 +10,13 @@ package labs_examples.lambdas.labs;
  *      10 of them! You can do it! You'll know so much about them when you're done!
  *
  */
+
+class Exercise_2{
+    public static void main(String[] args) {
+        DoubleToIntFunction df = (x) -> {
+            return (int)x*2;};
+        System.out.println("Double to Int Function: "+ df.applyAsInt(12.54));
+
+
+    }
+}
