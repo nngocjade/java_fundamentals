9cb5626680bc86852fe8759b9abc9f8a170467b1
nngocjade <nngocjade@gmail.com>
2020-02-24 11:33:47 +0700

    in progress

--- src/labs_examples/lambdas/labs/Exercise_02.java
+++ src/labs_examples/lambdas/labs/Exercise_02.java
@@ -1,5 +1,9 @@
 package labs_examples.lambdas.labs;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.DoubleToIntFunction;
 import java.util.function.Function;
 
@@ -13,10 +17,21 @@ import java.util.function.Function;
 
 class Exercise_2{
     public static void main(String[] args) {
+        //Double To Int Function
         DoubleToIntFunction df = (x) -> {
             return (int)x*2;};
         System.out.println("Double to Int Function: "+ df.applyAsInt(12.54));
 
+        //BiFunction - as a parameter
+        Calculator calculator = new Calculator();
+        String result = calculator.calc((a,b) -> "BiFunction - as a parameter: " + (a * b), 12, 5);
+        System.out.println(result);
 
+
+    }
+}
+class Calculator{
+    public String calc(BiFunction<Integer, Integer, String> bi, Integer i1, Integer i2){
+        return bi.apply(i1, i2);
     }
 }
