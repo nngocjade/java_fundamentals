35a34acca3da03d003e75c1455e2b1100f6d6aec
nngocjade <nngocjade@gmail.com>
2020-02-26 21:18:02 +0800

    in progress

--- src/labs_examples/lambdas/labs/Exercise_03.java
+++ src/labs_examples/lambdas/labs/Exercise_03.java
@@ -2,6 +2,8 @@ package labs_examples.lambdas.labs;
 
 import labs_examples.generics.labs.Exercise_03;
 
+import java.util.function.DoubleToIntFunction;
+
 /**
  * Lambdas Exercise 3:
  *
@@ -13,22 +15,24 @@ import labs_examples.generics.labs.Exercise_03;
 
 class Exercise_3 {
     public static void main(String[] args) {
-
+        //method reference
        Num n = DNum::dNumToIntNum;
-       n.num();
+       n.num(12.34, 23.45);
+
+       //Predicate Method Reference
+
 
 
     }
 }
 interface Num{
-    void num();
+    void num(double d1, double d2);
 }
 class DNum {
-
-    public static void dNumToIntNum () {
-        double d1 = 10;
-        double d2 = 12;
+    public static void dNumToIntNum (double d1, double d2) {
         int x = (int) (d1 * d2);
         System.out.println(x);
     }
+
+
 }
