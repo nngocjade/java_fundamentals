ccb17512918630bc9c72d877c8164a88c027f37c
nngocjade <nngocjade@gmail.com>
2020-02-26 22:48:31 +0800

    in progress

--- src/labs_examples/lambdas/labs/Exercise_03.java
+++ src/labs_examples/lambdas/labs/Exercise_03.java
@@ -16,8 +16,11 @@ import java.util.function.DoubleToIntFunction;
 class Exercise_3 {
     public static void main(String[] args) {
         //method reference
+        //example 1 - double to int
        Num n = DNum::dNumToIntNum;
        n.num(12.34, 23.45);
+        //example 2 - double to int using Functional Interface
+       DoubleToIntFunction d = DNum1::dNumToIntNum1;
 
        //Predicate Method Reference
 
@@ -33,6 +36,11 @@ class DNum {
         int x = (int) (d1 * d2);
         System.out.println(x);
     }
+}
 
-
+class DNum1{
+    public static int dNumToIntNum1 (double value){
+       int x = (int) (value*2);
+       return x;
+    }
 }
