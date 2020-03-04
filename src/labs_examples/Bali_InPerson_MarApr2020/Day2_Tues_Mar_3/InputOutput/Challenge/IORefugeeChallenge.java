341affd3e3c3391e7c4e027be38f07a9a796f70c
nngocjade <nngocjade@gmail.com>
2020-03-04 08:34:39 +0800

    almost done

--- src/labs_examples/Bali_InPerson_MarApr2020/Day2_Tues_Mar_3/InputOutput/Challenge/IORefugeeChallenge.java
+++ src/labs_examples/Bali_InPerson_MarApr2020/Day2_Tues_Mar_3/InputOutput/Challenge/IORefugeeChallenge.java
@@ -1,4 +1,4 @@
-package labs_examples.Bali_InPerson_MarApr2020.Day2_Mar_3.InputOutput.Challenge;
+package labs_examples.Bali_InPerson_MarApr2020.Day2_Tues_Mar_3.InputOutput.Challenge;
 
 //download this file
 import java.io.BufferedReader;
