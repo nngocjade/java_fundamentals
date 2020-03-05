5603af4b62376c5687433a82f04b33c7ab3c99b7
nngocjade <nngocjade@gmail.com>
2020-03-05 15:08:43 +0800

    done

--- src/labs_examples/Bali_InPerson_MarApr2020/Day4_Thurs_Mar_5/Lambda/Examples/MyGenericInterface.java
+++ src/labs_examples/Bali_InPerson_MarApr2020/Day4_Thurs_Mar_5/Lambda/Examples/MyGenericInterface.java
@@ -1,4 +1,20 @@
 package labs_examples.Bali_InPerson_MarApr2020.Day4_Thurs_Mar_5.Lambda.Examples;
 
+
 public class MyGenericInterface {
+
+    public static void main(String[] args) {
+
+        //Integer.parseInt() casts a String to an Integer if the String is a number, if not (a word for example) then it would not be able to cast/execute
+        SomeInterface<Double, Integer, String> someName = (Integer i, String s) -> (double) Integer.parseInt(s) + i;
+        double pen = someName.calc(7, "7");
+
+        System.out.println(pen);
+    }
 }
+
+@FunctionalInterface
+interface SomeInterface <N,S,J>{
+    public N calc (S val1, J val2);
+}
+
