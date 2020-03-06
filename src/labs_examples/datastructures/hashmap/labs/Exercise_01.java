067222af1619ad1bc832aacbfbfd07e96e66df0d
nngocjade <nngocjade@gmail.com>
2020-03-06 12:44:14 +0800

    in progress

--- src/labs_examples/datastructures/hashmap/labs/Exercise_01.java
+++ src/labs_examples/datastructures/hashmap/labs/Exercise_01.java
@@ -27,13 +27,64 @@ import java.util.HashMap;
 class HashMapsPractice{
     public static void main(String[] args) {
 
-        String [] anArray = {"Japan", "Africa", "United States", "Canada", "South America"};
 
-        HashMap<String, String> hashMap = new HashMap<>();
 
-        hashMap.put("Japan", "Narita");
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        hashMap.put("Japan", 32);
         hashMap.get("Japan");
+        hashMap.putAll();
+        hashMap.size();
+        hashMap.containsKey(32);
+        hashMap.keySet();
 
 
+
+
+
+    }
+}
+class Country{
+    String name;
+    String capital;
+    Double numOfPopulation;
+
+    public Country(String name, String capital, Double numOfPopulation) {
+        this.name = name;
+        this.capital = capital;
+        this.numOfPopulation = numOfPopulation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public Double getNumOfPopulation() {
+        return numOfPopulation;
+    }
+
+    public void setNumOfPopulation(Double numOfPopulation) {
+        this.numOfPopulation = numOfPopulation;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                ", capital='" + capital + '\'' +
+                ", numOfPopulation=" + numOfPopulation +
+                '}';
     }
 }
